6ab5fab4f2bbe688495baa80f079f8b2e47a4658
TurboMils <milan.pro01@hotmail.fr>
2022-04-14 10:01:02 +0200

    Build API v1

---
+++ src/main/java/com/bet3/usermanagement/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bet3.usermanagement.repository;
+
+import com.bet3.usermanagement.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+}
