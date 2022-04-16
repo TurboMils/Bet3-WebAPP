c32e488af3f0f3bd25df4464782e4701ec2b9e7f
TurboMils <milan.pro01@hotmail.fr>
2022-04-16 13:04:40 +0200

    Fix navbar

--- bet3-ui/components/Footer.js
+++ bet3-ui/components/Footer.js
@@ -8,44 +8,43 @@ export function Footer() {
       <div className={styles.branding}>
         <img src="/assets/images/logo.svg" draggable={false} />
         <div className={styles.links}>
-        <div className={styles.column}>
-          <b>App</b>
-          <a href={URLs.App}>Open App</a>
-          <a href={URLs.ProjectTracker}>
-            <a>Project Tracker</a>
-          </a>
-          <a>Download</a>
-          <a>Features</a>
-          <a>Branding</a>
-        </div>
-        <div className={styles.column}>
-          <b>Developers</b>
-          <a href={URLs.GitHub}>Contribute</a>
-          <a href={URLs.Developers}>Documentation</a>
-          <a href={URLs.Translate}>Help translate</a>
-        </div>
-        <div className={styles.column}>
-          <b>Team</b>
-          <Link href="/about">
-            <a>About Us</a>
-          </Link>
-          <a href="mailto:hello@revolt.chat">Email</a>
-        </div>
-        <div className={styles.column}>
-          <b>Legal</b>
-          <Link href="/terms">
-            <a>Terms of Service</a>
-          </Link>
-          <Link href="/privacy">
-            <a>Privacy Policy</a>
-          </Link>
-          <Link href="/aup">
-            <a>Acceptable Usage</a>
-          </Link>
+          <div className={styles.column}>
+            <b>App</b>
+            <a href={URLs.App}>Open App</a>
+            <a href={URLs.ProjectTracker}>
+              Project Tracker
+            </a>
+            <a>Download</a>
+            <a>Features</a>
+            <a>Branding</a>
+          </div>
+          <div className={styles.column}>
+            <b>Developers</b>
+            <a href={URLs.GitHub}>Contribute</a>
+            <a href={URLs.Developers}>Documentation</a>
+            <a href={URLs.Translate}>Help translate</a>
+          </div>
+          <div className={styles.column}>
+            <b>Team</b>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+            <a href="mailto:hello@revolt.chat">Email</a>
+          </div>
+          <div className={styles.column}>
+            <b>Legal</b>
+            <Link href="/terms">
+              <a>Terms of Service</a>
+            </Link>
+            <Link href="/privacy">
+              <a>Privacy Policy</a>
+            </Link>
+            <Link href="/aup">
+              <a>Acceptable Usage</a>
+            </Link>
+          </div>
         </div>
       </div>
-      </div>
-       
     </div>
   );
 }
--- bet3-ui/components/Navbar.js
+++ bet3-ui/components/Navbar.js
@@ -9,27 +9,21 @@ export function Navbar() {
     <div className={styles.navbar}>
       <div className='container mx-auto my-10'>
         <div className='flex shadow border-b'>
-          <table className=''>
-            <thead className='bg-gray-50'>
-              <Link href="/">
-                <a>
-                  <img className='h-20' src="/assets/images/logo3.svg" draggable={false} />
-                </a>
-              </Link>
-               </thead>
-          </table>
-              <div className={styles.actions}>
-                <a href={URLs.GitHub}>GitHub</a>
-                <a>Why BET3?</a>
-                <a href={URLs.Admin}> Administrator</a>
-                <a>Contribute</a>
-                <a>Request Early Access</a>
-              </div>
-           
+          <Link href="/">
+            <a><img className='h-20' src="/assets/images/logo3.svg" draggable={false} /></a>
+            
+          </Link>
+
+          <div className={styles.actions}>
+            <a href={URLs.GitHub}>GitHub</a>
+            <a>Why BET3?</a>
+            <a href={URLs.Admin}> Administrator</a>
+            <a>Contribute</a>
+            <a>Request Early Access</a>
+          </div>
         </div>
       </div>
     </div>
-
   );
 }
 
